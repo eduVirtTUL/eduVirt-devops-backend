4e1ed68c4d55281f3720c5ca135151fc5df21aa9
Adam Czerwonka <adamczerwonka59@gmail.com>
2024-10-23 18:20:52 +0200

    initial commit

---
+++ src/main/java/pl/lodz/p/it/eduvirt/EduVirtApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.it.eduvirt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduVirtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduVirtApplication.class, args);
+    }
+
+}
