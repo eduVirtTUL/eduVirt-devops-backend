de2839a66202137d4772c687ef7d3c831b7dcf71
Michał Piestrzeniewicz <michal.p2608@wp.pl>
2024-11-08 13:30:23 +0100

    Add: Added handling vnic profiles pool prototype

--- src/main/java/pl/lodz/p/it/eduvirt/controller/VnicProfileController.java
+++ src/main/java/pl/lodz/p/it/eduvirt/controller/VnicProfileController.java
@@ -5,12 +5,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.lodz.p.it.eduvirt.dto.VnicProfileDto;
+import pl.lodz.p.it.eduvirt.dto.VnicProfilePoolDto;
 import pl.lodz.p.it.eduvirt.service.OVirtVnicProfileService;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/resources/vnic-profiles")
@@ -19,11 +23,28 @@ public class VnicProfileController {
 
     private final OVirtVnicProfileService vnicProfileService;
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getVnicProfiles() {
+    @GetMapping(path = "/ovirt", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getFetchedVnicProfiles() {
         List<VnicProfileDto> vnicProfileDtoList = vnicProfileService.fetchVnicProfiles();
 
         if (vnicProfileDtoList.isEmpty()) return ResponseEntity.noContent().build();
         return ResponseEntity.ok(vnicProfileDtoList);
+    }
+
+    @GetMapping(path = "/eduvirt", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getVnicProfilesPool() {
+        List<VnicProfilePoolDto> vnicProfileDtoList = vnicProfileService.showVnicProfilePool();
+
+        if (vnicProfileDtoList.isEmpty()) return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(vnicProfileDtoList);
+    }
+
+    @PostMapping(path = "/eduvirt/add-to-pool/{vnicProfileId}")
+    public ResponseEntity<?> getVnicProfilesPool(@PathVariable("vnicProfileId") String vnicProfileId) {
+        //todo: handle mapping exception
+        boolean success = vnicProfileService.expandVnicProfilePool(UUID.fromString(vnicProfileId));
+
+        if (success) return ResponseEntity.noContent().build();
+        return ResponseEntity.badRequest().build();
     }
 }
--- src/main/java/pl/lodz/p/it/eduvirt/service/impl/OVirtVnicProfileServiceImpl.java
+++ src/main/java/pl/lodz/p/it/eduvirt/service/impl/OVirtVnicProfileServiceImpl.java
@@ -2,14 +2,21 @@ package pl.lodz.p.it.eduvirt.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.ovirt.engine.sdk4.Connection;
+import org.ovirt.engine.sdk4.types.VnicProfile;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pl.lodz.p.it.eduvirt.aspect.logging.LoggerInterceptor;
 import pl.lodz.p.it.eduvirt.dto.VnicProfileDto;
+import pl.lodz.p.it.eduvirt.dto.VnicProfilePoolDto;
+import pl.lodz.p.it.eduvirt.entity.network.VnicProfilePool;
 import pl.lodz.p.it.eduvirt.mappers.VnicProfileMapper;
+import pl.lodz.p.it.eduvirt.repository.VnicProfileRepository;
 import pl.lodz.p.it.eduvirt.service.OVirtVnicProfileService;
 import pl.lodz.p.it.eduvirt.util.connection.ConnectionFactory;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @LoggerInterceptor
@@ -18,6 +25,7 @@ public class OVirtVnicProfileServiceImpl implements OVirtVnicProfileService {
 
     private final ConnectionFactory connectionFactory;
     private final VnicProfileMapper vnicProfileMapper;
+    private final VnicProfileRepository vnicProfileRepository;
 
     @Override
     public List<VnicProfileDto> fetchVnicProfiles() {
@@ -35,5 +43,22 @@ public class OVirtVnicProfileServiceImpl implements OVirtVnicProfileService {
             //todo: error handling
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public List<VnicProfilePoolDto> showVnicProfilePool() {
+        return vnicProfileRepository.findAll().stream()
+                .map(VnicProfilePoolDto::fromEntity)
+                .toList();
+    }
+
+    @Override
+    @Transactional
+    public boolean expandVnicProfilePool(UUID vnicProfileId) {
+        //todo maybe change returning boolean to throwing an exception "entity exists"
+        if (vnicProfileRepository.findById(vnicProfileId).isPresent()) return false;
+
+        vnicProfileRepository.save(new VnicProfilePool(vnicProfileId, false));
+        return true;
     }
 }
