96d79bdf2a1a74783dc54b38e53a6b6794b674f7
Michał Piestrzeniewicz <michal.p2608@wp.pl>
2024-12-02 21:03:47 +0100

    Add: Additional endpoint for VnicProfileController to gettig vnic profile by id

--- src/main/java/pl/lodz/p/it/eduvirt/dto/vnic_profile/VnicProfilePoolMemberDto.java
+++ src/main/java/pl/lodz/p/it/eduvirt/dto/vnic_profile/VnicProfilePoolMemberDto.java
@@ -1,17 +1,7 @@
 package pl.lodz.p.it.eduvirt.dto.vnic_profile;
 
-import pl.lodz.p.it.eduvirt.entity.eduvirt.network.VnicProfilePoolMember;
-
-import java.util.Optional;
-
 public record VnicProfilePoolMemberDto(
         String id,
+        String name,
         boolean inUse
-) {
-
-//    public static VnicProfilePoolMemberDto fromEntity(VnicProfilePoolMember entity) {
-//        return Optional.ofNullable(entity)
-//                .map(e -> new VnicProfilePoolMemberDto(e.getId().toString(), e.getInUse()))
-//                .orElse(null);
-//    }
-}
+) { }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO public.i72_private_vlans_range (range_from, range_to, id) VALUES (0, 4096, '0978f66d-050c-4c28-a376-9b8934d6167a');
